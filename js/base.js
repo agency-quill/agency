5f1b1b57889be99f5361a3153ed8b5183083d84f
edmonan <ae@edmonan.com>
2018-09-18 09:21:16 -0500

    update qplus templates and .js

--- js/base.js
+++ js/base.js
@@ -153,8 +153,8 @@ function dropDown(target, action){
 	}
 }
 function fixDynPrice(node, price){
-	if(price && /\{\{dynPrice\}\}/.test(node.html())){
-		node.html().replace(/\{\{dynPrice\}\}/, price);
+	if(price && node.html().indexOf('{{dynPrice}}') >= 0){
+		node.html(node.html().substring(0, node.html().indexOf('{{dynPrice}}')) + price + node.html().substring(node.html().indexOf('{{dynPrice}}') + 12));
 	}
 }
 function getParamVal(string, param){
