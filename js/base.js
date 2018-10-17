f2e90676e279eaa2f57f6b425b202169c4babd28
Seyi Owolabi <seyi.owolabi@quill.com>
2018-10-17 08:02:37 -0500

    changed img class for custom print page

--- js/base.js
+++ js/base.js
@@ -67,7 +67,7 @@ function buildCustomPrint(arr) {
     for(var i = 0, max = arr.length; i < max; i++){
         var ob = function(e){return e.hasOwnProperty('productPriceStart'); }
         if (arr.some(ob)){
-            out += "<div class=\"grid__unit--25\"><div class=\"align--center\">" + "<a class=\"a--noHighlight\" href=\"" + arr[i].productUrl + "\"><picture class=\"align--center\"><img src=\"" + arr[i].productImage +"\" alt=\"" + arr[i].productName + "\" class=\"img margin__bottom--25\"></picture>" + "<h4 class=\"h4 margin__bottom--0\">" + arr[i].productName + "</h4>" + "<p class=\"p--small\">" + arr[i].productPriceStart + "</p></a></div></div>";
+            out += "<div class=\"grid__unit--25\"><div class=\"align--center\">" + "<a class=\"a--noHighlight\" href=\"" + arr[i].productUrl + "\"><picture class=\"align--center\"><img src=\"" + arr[i].productImage +"\" alt=\"" + arr[i].productName + "\" class=\"webModImg__webModCard--25 margin__bottom--25\"></picture>" + "<h4 class=\"h4 margin__bottom--0\">" + arr[i].productName + "</h4>" + "<p class=\"p--small\">" + arr[i].productPriceStart + "</p></a></div></div>";
             }
         else{
                 out += "<div class=\"grid__unit--25\"><div class=\"align--center\">" + "<a class=\"a--noHighlight\" href=\"" + arr[i].productUrl + "\"><picture class=\"align--center\"><img src=\"" + arr[i].productImage +"\" alt=\"" + arr[i].productName + "\" class=\"img margin__bottom--25\"></picture>" + "<h4 class=\"h4 margin__bottom--50\">" + arr[i].productName + "</h4></a></div></div>";
